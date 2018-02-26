9b9a2ffca34c0fa1abf937339ff74a8709777a63
Daniel Kim <dskay7@gmail.com>
2018-02-25 22:14:39 -0500

    initial files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/App";
+
+import reducers from "./reducers";
+
+import "./index.css";
+
+const createStoreWithMiddleWare = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleWare(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
