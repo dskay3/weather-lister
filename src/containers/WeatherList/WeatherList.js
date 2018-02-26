a7e69b18f7adfc2eae35ad311614193698aa47e1
Daniel Kim <dskay7@gmail.com>
2018-02-26 17:27:51 -0500

    added charts

--- src/containers/WeatherList/WeatherList.js
+++ src/containers/WeatherList/WeatherList.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import Chart from "../../components/Chart";
+
 class WeatherList extends Component {
   renderWeather(cityData) {
     const cityName = cityData.city.name;
+    const temp = cityData.list.map(weather => weather.main.temp);
+    const humidities = cityData.list.map(humidity => humidity.main.humidity);
+    const pressures = cityData.list.map(pressure => pressure.main.pressure);
 
     return (
       <tr key={cityName}>
         <td>{cityName}</td>
+        <td>
+          <Chart data={temp} color="red" units="K" />
+        </td>
+        <td>
+          <Chart data={pressures} color="green" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="black" units="%" />
+        </td>
       </tr>
     );
   }
@@ -18,9 +32,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
 
